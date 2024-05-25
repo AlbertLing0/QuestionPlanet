25fa100c73a70d3d0e4b380358b2e5eedb9c05a4
yranium <2647169392@qq.com>
2024-05-25 16:48:03 +0800

    视差背景在不同界面间连贯显示

--- front-end/src/pages/login.vue
+++ front-end/src/pages/login.vue
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export default defineComponent({
   components: {
-    PlanetBG,NavBar,axios
+    NavBar,axios
   },
 
   setup() {
@@ -46,9 +46,6 @@ export default defineComponent({
 <template>
   <div id="naviBar">
     <nav-bar/>
-  </div>
-  <div id="BG">
-    <planet-b-g/>
   </div>
   <div  class="loginBox">
     <div class="title">
